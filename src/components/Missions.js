7630ace568ea9ce029463f49691b2008cfd6c999
Thiago Reis Nascimento <trnasci@gmail.com>
2022-08-19 16:16:53 -0300

    Correção dos arquivos css

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,19 +2,23 @@ import React from 'react';
 import Title from './Title';
 import missions from '../data/missions';
 import MissionCard from './MissionCard';
+import './Missions.css';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions" className="missions">
+      <div data-testid="missions">
         <Title headline="Missões" />
-        {missions.map((mission) => (<MissionCard
-          key={ mission.name }
-          name={ mission.name }
-          year={ mission.year }
-          country={ mission.country }
-          destination={ mission.destination }
-        />))}
+        <span className="missions">
+          {missions.map((mission) => (<MissionCard
+            key={ mission.name }
+            className="mission-card"
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />))}
+        </span>
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -2,17 +2,20 @@ import React from 'react';
 import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
+import './SolarSystem.css';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system" className="solar-system">
         <Title headline="Planetas" />
-        {planets.map((planet) => (<PlanetCard
-          key={ planet.name }
-          planetName={ planet.name }
-          planetImage={ planet.image }
-        />))}
+        <span className="planets">
+          {planets.map((planet) => (<PlanetCard
+            key={ planet.name }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />))}
+        </span>
       </div>);
   }
 }
