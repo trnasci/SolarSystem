b23352bb2f36eec62d1d33764e1be061389f5c9d
Thiago Reis Nascimento <trnasci@gmail.com>
2022-08-20 17:09:11 -0300

    Correção do requisito 5

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './PlanetCard.css';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
-        <img scr={ planetImage } alt={ `Planeta ${planetName}` } />
-        <p data-testid="planet-name">
+      <div data-testid="planet-card" className="planet-card">
+        <img
+          className="img-planet-card"
+          id={ planetName }
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+        />
+        <p data-testid="planet-name" className="planet-name">
           {planetName}
         </p>
       </div>
